a2b7722d8fe5beb72ba974a2d2949ab8047acaac
Meetteem <124702327+Meetteem@users.noreply.github.com>
2025-02-20 12:02:10 +0530

    Update route.js

--- src/app/api/symptom_diagnosis/route.js
+++ src/app/api/symptom_diagnosis/route.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 // Google Gemini API Endpoint
 const GEMINI_API_URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent";
-const API_KEY = process.env.NEXT_PUBLIC_GEMINI_API_KEY;
+const API_KEY = AIzaSyC3dp77y-3aTST9RJTuqZKtLHAAWvr052M;
 
 export async function POST(req) {
   try {
